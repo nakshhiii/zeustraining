79a1a6dd9402a76d9ea9bba903ef057178980cf3
Nakshi <nakshishahh@gmail.com>
2024-01-17 11:07:42 +0530

    final changes

--- quantumlogin/dashboardjs/script/index.js
+++ quantumlogin/dashboardjs/script/index.js
@@ -1,14 +1,9 @@
 function generateOptions(list) {
     var res = '';
-    for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
-        var item = list_1[_i];
-        if (typeof item === 'string') {
-            // Check if item is a string
+    for (var i = 0, list_1 = list; i < list_1.length; i++) {
+        var item = list_1[i];
             res += "<option value=\"".concat(item, "\">").concat(item, "</option>");
-        } else if (item.value && item.label) {
-            // Check if item is an object with 'value' and 'label' properties
-            res += "<option value=\"".concat(item.value, "\">").concat(item.label, "</option>");
-        }
+        
     }
     return res;
 }
@@ -19,26 +14,75 @@ var renderCard = function () {
     // Fetch data asynchronously
     fetch("/zeustraining/quantumlogin/dashboardjs/script/data.json")
         .then(function (response) {
-            // Convert the response to JSON
+           
             return response.json();
         })
         .then(function (datalist) {
-            // Now we have the data, so proceed with rendering
             courseCount.innerText = datalist.length;
 
-            for (var _i = 0, datalist_1 = datalist; _i < datalist_1.length; _i++) {
-                var data = datalist_1[_i];
+            for (var i = 0; i < datalist.length; i++) {
+                var data = datalist[i];
                 var card = document.createElement('div');
                 card.classList.add('card');
-                card.innerHTML += "\n        \n                    ".concat(data.isAlive ? '' : "<span class=\"expireTag\">EXPIRED</span>", "\n                    <img src=\"").concat(data.profilePic, "\" alt=\"\" class=\"card-img\">\n                    <div class=\"details\">\n                        <div class=\"space-between mb-7\">\n                            <p class=\"detail-heading bold\">").concat(data.title, "</p>\n                            ").concat(data.isFavorite ? "<img src=\"assets/icons/favourite.svg\" alt=\"\" srcset=\"\">" : "<img src=\"assets/icons/favourite.svg\" alt=\"\" srcset=\"\">", "\n                        </div>\n\n                        <div class=\"sub-info\">\n                            <p style=\"color: #666666; font-size: 12px;\" class=\"mb-7\">").concat(data.subject, " Grade <span style=\"color: #1F7A54;\">").concat(data.grade, "</span></p>\n                            ").concat(data.details ? "<p style=\"color: #666666; font-size: 12px;\" class=\"mb-18\"> <span class=\"dark bold\">".concat(data.details.units, "</span> Units <span class=\"dark bold\">").concat(data.details.lessons, "</span> lessons <span class=\"dark bold\">").concat(data.details.topics, "</span> topics</p>") : '', "\n                        </div>\n\n                        <div class=\"teacher-info\">\n                            <div class=\"space-between\">\n                                <select name=\"class\" class=\"class\">\n                                    ").concat(generateOptions(data.classOption), "\n                                </select>\n                            </div>\n                        </div>\n\n                        <div class=\"course-info\">\n                            ").concat(data.students ? "<span style=\"margin-right: 17px;\">".concat(data.students, " Students</span> <span>").concat(data.stardDate ? data.stardDate : '', " - ").concat(data.endDate ? data.endDate : '', "</span>") : '', "\n                        </div>\n                    </div>\n                    <div class=\"card-icons\">\n                        <button ").concat(data.optionButton.preview ? 'disabled class="card-btn-enable"' : '', "> <img src=\"assets/icons/preview.svg\" alt=\"\" srcset=\"\"></button>\n                        <button ").concat(data.optionButton.manageCourse ? 'disabled class="card-btn-enable"' : '', "> <img src=\"assets/icons/manage course.svg\" alt=\"\" srcset=\"\"></button>\n                        <button ").concat(data.optionButton.gradeSubmissions ? 'disabled class="card-btn-enable"' : '', "> <img src=\"assets/icons/grade submissions.svg\" alt=\"\" srcset=\"\"></button>\n                        <button ").concat(data.optionButton.reports ? 'disabled class="card-btn-enable"' : '', "> <img src=\"assets/icons/reports.svg\" alt=\"\" srcset=\"\"></button>\n                    </div>\n                    ");
+                card.innerHTML += `
+                ${data.isAlive ? '' : '<span class="expireTag">EXPIRED</span>'}
+                <img src="${data.profilePic}" alt="" class="card-img">
+                <div class="details">
+                    <div class="space-between mb-7">
+                        <p class="detail-heading bold">${data.title}</p>
+                        ${data.isFavorite ? '<img src="assets/icons/favourite.svg" alt="" srcset="">' : '<img src="assets/icons/favourite.svg" alt="" srcset="">'}
+                    </div>
+            
+                    <div class="sub-info">
+                        <p style="color: #666666; font-size: 12px;" class="mb-7">
+                            ${data.subject} Grade <span style="color: #1F7A54;">${data.grade}</span>
+                        </p>
+                        ${data.details ? `<p style="color: #666666; font-size: 12px;" class="mb-18">
+                            <span class="dark bold">${data.details.units}</span> Units 
+                            <span class="dark bold">${data.details.lessons}</span> lessons 
+                            <span class="dark bold">${data.details.topics}</span> topics
+                        </p>` : ''}
+                    </div>
+            
+                    <div class="teacher-info">
+                        <div class="space-between">
+                            <select name="class" class="class">
+                                ${generateOptions(data.classOption)}
+                            </select>
+                        </div>
+                    </div>
+            
+                    <div class="course-info">
+                        ${data.students ? `
+                            <span style="margin-right: 17px;">${data.students} Students</span> 
+                            <span>${data.stardDate ? data.stardDate : ''} - ${data.endDate ? data.endDate : ''}</span>` : ''}
+                    </div>
+                </div>
+            
+                <div class="card-icons">
+                    <button ${data.optionButton.preview ? ' class="card-btn-enable"' : ''}>
+                        <img src="assets/icons/preview.svg" alt="" srcset="">
+                    </button>
+                    <button ${data.optionButton.manageCourse ? ' class="card-btn-enable"' : ''}>
+                        <img src="assets/icons/manage course.svg" alt="" srcset="">
+                    </button>
+                    <button ${data.optionButton.gradeSubmissions ? 'class="card-btn-enable"' : ''}>
+                        <img src="assets/icons/grade submissions.svg" alt="" srcset="">
+                    </button>
+                    <button ${data.optionButton.reports ? ' class="card-btn-enable"' : ''}>
+                        <img src="assets/icons/reports.svg" alt="" srcset="">
+                    </button>
+                </div>
+            `;
+            
                 display.appendChild(card);
             }
         })
         .catch(function (error) {
-            // Handle errors if any
+            
             console.error('Error fetching data:', error);
         });
 };
 
-// Call the renderCard function
+
 renderCard();
