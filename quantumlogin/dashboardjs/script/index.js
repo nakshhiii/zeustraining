990ff59c22cffbef086273e6e746011fa2c6e9f4
Nakshi <nakshishahh@gmail.com>
2024-01-17 15:59:06 +0530

    final changes

--- quantumlogin/dashboardjs/script/index.js
+++ quantumlogin/dashboardjs/script/index.js
@@ -21,68 +21,71 @@ var renderCard = function () {
             courseCount.innerText = datalist.length;
 
             for (var i = 0; i < datalist.length; i++) {
-                var data = datalist[i];
-                var card = document.createElement('div');
+                let data = datalist[i];
+                let card = document.createElement('div');
                 card.classList.add('card');
-                card.innerHTML += `
-                ${data.isAlive ? '' : '<span class="expireTag">EXPIRED</span>'}
-                <img src="${data.profilePic}" alt="" class="card-img">
-                <div class="details">
-                    <div class="space-between mb-7">
-                        <p class="detail-heading bold">${data.title}</p>
-                        ${data.isFavorite ? '<img src="assets/icons/favourite.svg" alt="" srcset="">' : '<img src="assets/icons/favourite.svg" alt="" srcset="">'}
-                    </div>
+               card.innerHTML += `
+    ${data.isExpired ? '' : '<span class="expireTag">EXPIRED</span>'}
+    <img src="${data.profilePic}" alt="" class="card-img">
+    <div class="details">
+        <div class="space-between mb-7">
+            <p class="detail-heading bold">${data.title}</p>
+            ${data.isFavorite ? '<img src="assets/icons/favourite.svg" alt="" srcset="">' : ''}
+        </div>
+
+        <div class="sub-info">
+            <p style="color: #666666; font-size: 12px;" class="mb-7">
+                ${data.subject} Grade <span style="color: #1F7A54;">${data.grade} + ${data.subgrade}</span>
+            </p>
+            ${data.details ? `<p style="color: #666666; font-size: 12px;" class="mb-18">
+                <span class="dark bold">${data.details.units}</span> Units 
+                <span class="dark bold">${data.details.lessons}</span> lessons 
+                <span class="dark bold">${data.details.topics}</span> topics
+            </p>` : ''}
+        </div>
+
+        <div class="teacher-info">
+            <div class="space-between">
+                <select name="class" class="class">
+                    ${generateOptions(data.classOption)}
+                </select>
+            </div>
+        </div>
+        <div class="course-info">
+        ${data.students ? `
+            <span style="margin-right: 17px;">${data.students} Students</span> 
             
-                    <div class="sub-info">
-                        <p style="color: #666666; font-size: 12px;" class="mb-7">
-                            ${data.subject} Grade <span style="color: #1F7A54;">${data.grade}</span>
-                        </p>
-                        ${data.details ? `<p style="color: #666666; font-size: 12px;" class="mb-18">
-                            <span class="dark bold">${data.details.units}</span> Units 
-                            <span class="dark bold">${data.details.lessons}</span> lessons 
-                            <span class="dark bold">${data.details.topics}</span> topics
-                        </p>` : ''}
-                    </div>
-            
-                    <div class="teacher-info">
-                        <div class="space-between">
-                            <select name="class" class="class">
-                                ${generateOptions(data.classOption)}
-                            </select>
-                        </div>
-                    </div>
-            
-                    <div class="course-info">
-                        ${data.students ? `
-                            <span style="margin-right: 17px;">${data.students} Students</span> 
-                            <span>${data.stardDate ? data.stardDate : ''} - ${data.endDate ? data.endDate : ''}</span>` : ''}
-                    </div>
-                </div>
-            
-                <div class="card-icons">
-                    <button ${data.optionButton.preview ? ' class="card-btn-enable"' : ''}>
-                        <img src="assets/icons/preview.svg" alt="" srcset="">
-                    </button>
-                    <button ${data.optionButton.manageCourse ? ' class="card-btn-enable"' : ''}>
-                        <img src="assets/icons/manage course.svg" alt="" srcset="">
-                    </button>
-                    <button ${data.optionButton.gradeSubmissions ? 'class="card-btn-enable"' : ''}>
-                        <img src="assets/icons/grade submissions.svg" alt="" srcset="">
-                    </button>
-                    <button ${data.optionButton.reports ? ' class="card-btn-enable"' : ''}>
-                        <img src="assets/icons/reports.svg" alt="" srcset="">
-                    </button>
-                </div>
-            `;
-            
+            ${data.startDate ? `<span style="padding-right:15px;"  > | </span> <span>${data.startDate} - ${data.endDate}</span>` : ''}
+        ` : ''}
+    </div>
+    
+</div>
+    </div>
+
+    <div class="card-icons">
+        <button ${data.optionButton.preview ? 'disabled class="card-btn-enable"' : ''}>
+            <img src="assets/icons/preview.svg" alt="" srcset="">
+        </button>
+        <button ${data.optionButton.manageCourse ? 'disabled class="card-btn-enable"' : ''}>
+            <img src="assets/icons/manage course.svg" alt="" srcset="">
+        </button>
+        <button ${data.optionButton.gradeSubmissions ? 'disabled class="card-btn-enable"' : ''}>
+            <img src="assets/icons/grade submissions.svg" alt="" srcset="">
+        </button>
+        <button ${data.optionButton.reports ? 'disabled class="card-btn-enable"' : ''}>
+            <img src="assets/icons/reports.svg" alt="" srcset="">
+        </button>
+    </div>
+`;
+
                 display.appendChild(card);
             }
         })
         .catch(function (error) {
-            
+            // Handle errors if any
             console.error('Error fetching data:', error);
         });
 };
 
-
+// Call the renderCard function
 renderCard();
